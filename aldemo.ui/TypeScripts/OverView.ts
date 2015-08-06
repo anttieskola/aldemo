8bd037e6fc028158ed9379362ba20bea5f3c7575
Antti Eskola <antti.eskola@gmail.com>
2015-08-06 18:38:05 +0300

    VS still does not handle git repository :panda_face:

---
+++ aldemo.ui/TypeScripts/OverView.ts
@@ -0,0 +1,55 @@
+﻿import bb = Backbone;
+import models = require("Models");
+import statusViews = require("StatusViews");
+
+// overview has no real model so making it a normal class
+export class OverView {
+    // dom element to draw upon (naming it same way as backbone)
+    private el: JQuery;
+    // we need collections of projects, lines and status on those lines
+    private projects: models.Projects;
+    private lines: models.Lines;
+    private statuses: models.Statuses;
+
+    // constructor
+    constructor(elementId: string) {
+        // set element to draw upon
+        this.el = $('#' + elementId);
+        // render when all collections loaded
+        models.Messaging().on("loadComplete", this.render, this);
+    }
+
+    // render view (naming like backbone)
+    public render() {
+        var table = $('<table>');
+        var row = $('<tr style="height: 125px;">');
+        row.append($('<td>')); // empty colum for project name
+        models.Collections().Lines.each(l => {
+            var cell = $('<td style="-webkit-transform: rotate(-90deg); max-width: 25px;">');
+            cell.attr('data-line-id', l.id);
+            cell.text(l.Name);
+            row.append(cell);
+        }, this);
+        table.append(row);
+
+        models.Collections().Projects.each(p => {
+            // row and project name
+            var tr = $('<tr>');
+            tr.attr('data-project-id', p.id);
+            var td = $('<td>');
+            td.text(p.Name);
+            tr.append(td);
+            // status
+            models.Collections().Statuses.ByProjectId(p.id).each(s => {
+                var td = $('<td>');
+                var sv = new statusViews.StatusView(s);
+                td.append(sv.render().el);
+                tr.append(td);
+            }, this);
+            // add row to table
+            table.append(tr);
+        }, this);
+        this.el.html(""); // clean (when all loading ready)
+        this.el.append(table); // add
+    }
+}
---
+++ aldemo.ui/TypeScripts/StatusViews.ts
@@ -0,0 +1,65 @@
+﻿import bb = Backbone;
+import models = require("Models");
+
+export class StatusView extends bb.View<models.Status> {
+    private template: (data: any) => string;
+    constructor(model: models.Status) {
+        // model
+        this.model = model;
+        // template from index.html
+        this.template = _.template($('#status-template').html());
+        super();
+    }
+    render(): bb.View<models.Status> {
+        this.$el.html(this.template(this.model.toJSON()));
+        // events
+        this.undelegateEvents();
+        this.delegateEvents({
+            "contextmenu .statusDiv": "openMenu"
+        });
+        return this;
+    }
+    // context menu
+    openMenu(event) {
+        // prevent normal context menu popping up (might not be cross-browser)
+        event.preventDefault();
+        // find cursor position
+        var left = event.clientX;
+        var top = event.clientY;
+        // open menu at position
+        var menu = $('#menu');
+        menu.css('left', left);
+        menu.css('top', top);
+        menu.show();
+        // need to hook-up to menu, note using proxy so context stays inside class
+        menu.on('mouseout', $.proxy(this.moveOut, this));
+        menu.on('click', $.proxy(this.clickMenu, this));
+    }
+    // mouse moving outside menu
+    moveOut(event) {
+        // need to check is mouse in one of the child elements of menu
+        var menu = $('#menu');
+        var current = $(event.toElement);
+        if (menu.find(current).length < 1) {
+            this.closeMenu();
+        }
+    }
+    // click in the menu
+    clickMenu(event) {
+        var e = $(event.toElement);
+        var newColor = e.text();
+        this.model.Text = newColor;
+        this.model.save();
+        this.render();
+        this.closeMenu();
+    }
+    // close menu
+    closeMenu() {
+        var menu = $('#menu');
+        // it is outside children, so hide
+        menu.hide();
+        // un-hook
+        menu.off('mouseout', this.closeMenu);
+        menu.off('click', this.clickMenu);
+    }
+}
